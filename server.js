1907f410e6aff37048b6b6b449e3826bb6c75a17
Rohitsinghwho <rohiisingh8655@gmail.com>
2023-08-08 05:40:07 +0530

    Added notes schema and model and also updated database.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+// importing express from express;
 const express= require('express');
+// importing database from database;
 const connecttoDb=require('./Database/database.js')
 const app= express();
-
+app.use(express.json());
+// connecting to database
+connecttoDb();
+// listening on port
 app.listen(5000,()=>{
     console.log("App is running on http://localhost:5000");
 })
 
-connecttoDb();
+// Routes
+app.get('api/notes/',require()) ;
