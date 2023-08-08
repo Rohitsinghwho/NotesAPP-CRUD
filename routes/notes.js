9ed6ebd3bb58b9ad4089c492bdf8574167ddd0c4
Rohitsinghwho <rohiisingh8655@gmail.com>
2023-08-08 06:18:28 +0530

    Created first api to add notes in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,18 @@
+const notesModel = require("../models/notesschema.js");
+const express = require("express");
 
+const router = express.Router();
+
+// Route =1  , creating notes on api api/notes/addNotes
+router.get("/addNotes", async (req, res) => {
+  const notes = new notesModel({
+    title: req.body.title,
+    Description: req.body.Description,
+  });
+  // save to database
+  const savedNotes = await notes.save();
+  // display as json data
+  res.json(savedNotes);
+});
+
+module.exports = router;
